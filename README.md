f3a01981118371c196b62da83f00add724bc9ba1
Lyndon Maydwell <lyndon@sordina.net>
2014-10-27 13:18:08 +1100

    Adding readme for slides

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
-# tardistest
+# The Tardis Monad
 
-A work in progress...
+![Tardis](http://hello.eboy.com/eboy/wp-content/uploads/2013/10/PT-Tardis-01t-6x.png)
+
+Tardis (or TardisT) provides the capabilities of both the State, and the Reverse-State monads in one handy package.
+
+	 cabal install tardis
+	 
+With this package you have several monadic functions available.
+
+	 class (Control.Applicative.Applicative m, Control.Monad.Fix.MonadFix m) =>
+	       MonadTardis bw fw (m :: * -> *) | m -> bw, m -> fw where
+	   getPast :: m fw
+	   getFuture :: m bw
+	   sendPast :: bw -> m ()
+	   sendFuture :: fw -> m ()
+	   tardis :: ((bw, fw) -> (a, (bw, fw))) -> m a
+
+	 type Tardis bw fw = TardisT bw fw Data.Functor.Identity.Identity
+	 type role TardisT nominal nominal representational nominal
+	 newtype TardisT bw fw (m :: * -> *) a
+	       = Control.Monad.Trans.Tardis.TardisT {runTardisT :: (bw, fw) -> m (a, (bw, fw))}
+
+	 evalTardis      :: Tardis bw fw a -> (bw, fw) -> a
+	 evalTardisT     :: Monad m => TardisT bw fw m a -> (bw, fw) -> m a
+	 execTardis      :: Tardis bw fw a -> (bw, fw) -> (bw, fw)
+	 execTardisT     :: Monad m => TardisT bw fw m a -> (bw, fw) -> m (bw, fw)
+	 getsFuture      :: MonadTardis bw fw m => (bw -> a) -> m a
+	 getsPast        :: MonadTardis bw fw m => (fw -> a) -> m a
+	 modifyBackwards :: MonadTardis bw fw m => (bw -> bw) -> m ()
+	 modifyForwards  :: MonadTardis bw fw m => (fw -> fw) -> m ()
+	 noState         :: (a, b)
+	 runTardis       :: Tardis bw fw a -> (bw, fw) -> (a, (bw, fw))
+
+The most important being...
+
+* getPast
+* getFuture
+* sendPast
+* sendFuture
+
+These allow you to work with the state monad
+
+* getPast
+* sendFuture
+
+and the reverse-state monad
+
+* getFuture
+* sendPast
+
+at the same time.
+
+# State Example
+
+	state_example = do
+		sendFuture 123
+		x <- getPast
+		return (x + 1)
+	
+	runTardis state_example ((),5) -- => (124,((),123))
+
+# Reverse State Example
+
+
+
+# Other Links
+
+* [Temporally Quaquaversal Virtual Nanomachine Programming In Multiple Topologically Connected Quantum-Relativistic Parallel Timespaces...Made Easy!](http://blip.tv/oreilly-open-source-convention/oscon-2008-damian-conway-thoughtstream-temporally-quaquaversal-virtual-nanomachine-programming-in-multiple-t-1151669)
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,107 @@
 module Main where
 
+  -- http://hackage.haskell.org/package/tardis-0.3.0.0/docs/Control-Monad-Tardis.html
+
+import Control.Monad.Tardis
+import Data.Maybe
+
 main :: IO ()
-main = print "hello world"
+main = timeParadox
+
+newtype Person       = Person { unPerson :: String } deriving (Eq, Show)
+newtype Instruction  = Kill   { who      :: Person } deriving (Eq, Show)
+
+type Instructions    = [Instruction]
+type Living          = [Person]
+type Paradoxically a = Tardis Instructions Living a
+
+timeParadox :: IO ()
+timeParadox = print $ evalTardis paradox ([], [june,pete,jane,joe])
+
+june, pete, jane, joe :: Person
+june = Person "Grandma June"
+pete = Person "Grandpa Pete"
+jane = Person "Grandma Jane"
+joe  = Person "Grandpa Joe"
+
+paradox :: Paradoxically Bool
+paradox = do
+  patGrams  <- getPerson june
+  patGramps <- getPerson pete
+  matGrams  <- getPerson jane
+  matGramps <- getPerson joe
+
+  inst <- getInstructions
+  -- respondToInstructions inst
+  ---- Ignore instruction and kill pete
+  killPete
+
+  dad       <- breed patGrams patGramps
+  mom       <- breed matGrams matGramps
+  maybeMe   <- breed dad mom
+
+  if isJust maybeMe then sendInstructionBackThroughTime (Kill pete)
+                    else return ()
+
+  amIAlive
+
+respondToInstructions :: [Instruction] -> Paradoxically ()
+respondToInstructions ls = mapM_ respondToInstruction ls
+
+respondToInstruction :: Instruction -> Paradoxically ()
+respondToInstruction (Kill person) | person == pete = killPete
+respondToInstruction (Kill _     )                  = return ()
+
+breed :: Maybe Person -> Maybe Person -> Paradoxically (Maybe Person)
+breed (Just x) (Just y) = do
+  px <- getPerson x
+  py <- getPerson y
+  breedDumb px py
+breed _ _ = return Nothing
+
+breedDumb :: Maybe Person -> Maybe Person -> Paradoxically (Maybe Person)
+breedDumb (Just x) (Just y) = do
+  let newPerson = namePair x y
+  personIsBorn newPerson
+  return $ Just $ newPerson
+breedDumb _ _ = return Nothing
+
+namePair :: Person -> Person -> Person
+namePair (Person x) (Person y) = Person $ concat ["(", x, " + ", y, ")"]
+
+personIsBorn :: Person -> Paradoxically ()
+personIsBorn person = modifyForwards (person : )
+
+getPerson :: Person -> Paradoxically (Maybe Person)
+getPerson person = do
+  people <- getPast
+  if elem person people
+     then return (Just person)
+     else return Nothing
+
+killPete :: Paradoxically ()
+killPete = killPerson pete
+
+getInstructions :: Paradoxically Instructions
+getInstructions = getFuture
+
+sendInstructionBackThroughTime :: Instruction -> Paradoxically ()
+sendInstructionBackThroughTime i = modifyBackwards (i : )
+
+killPerson :: Person -> Paradoxically ()
+killPerson person = modifyForwards (filter (/= person))
+
+amIAlive :: Paradoxically Bool
+amIAlive = do
+  people <- getPast
+  return $ elem (namePair (namePair june pete) (namePair jane joe)) people
+
+{-
+  patGrams  <- getPerson june
+  patGramps <- getPerson pete
+  matGrams  <- getPerson jane
+  matGramps <- getPerson joe
+  dad       <- breed patGrams patGramps
+  mom       <- breed matGrams matGramps
+  maybeMe   <- breed dad mom
+  -}
